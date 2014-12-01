68f1db91ecaac24d40f9d016805f57a0680fe626
Andy Chu <andychu@google.com>
2014-12-01 15:42:28 -0800

    find_cliques.cc: implementation of part of the algorithm for unknown dictionaries in C++.  See README and comments for details.

---
+++ analysis/cpp/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Usage:
+#   ./run.sh <function name>
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+build() {
+  # for unordered_{map,set}
+  g++ -std=c++0x -o find_cliques find_cliques.cc 
+}
+
+test-bad-edge() {
+  # Edge should go from lesser partition number to greater
+  ./find_cliques <<EOF
+num_partitions 3
+edge 1.ab 0.cd
+EOF
+}
+
+demo() {
+  local graph=${1:-testdata/graph1.txt}
+  build
+
+  time cat $graph | ./find_cliques
+}
+
+"$@"

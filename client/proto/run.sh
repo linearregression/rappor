5d92bf52f2417ba46feb546a928e1d745d0f0aed
Andy Chu <andychu@google.com>
2015-03-12 14:27:30 -0700

    Get rid of old stuff

--- client/proto/run.sh
+++ client/proto/run.sh
@@ -12,6 +12,10 @@ setup() {
   sudo apt-get install protobuf-compiler libprotobuf-dev
 }
 
+init() {
+  mkdir --verbose -p _tmp
+}
+
 rappor-test() {
   make _tmp/rappor_test
   _tmp/rappor_test "$@"
@@ -33,37 +37,6 @@ hmac-drbg-test() {
 hmac-openssl-test() {
   make _tmp/hmac_openssl_test
   _tmp/hmac_openssl_test
-}
-
-get() {
-  wget --no-clobber --directory _tmp "$@"
-}
-
-download() {
-  get https://chromium.googlesource.com/chromium/src/+archive/master/components/rappor.tar.gz
-  get https://chromium.googlesource.com/chromium/src/+archive/master/crypto.tar.gz
-}
-
-extract() {
-  mkdir -p _tmp/chrome _tmp/crypto
-
-  pushd _tmp/chrome
-  tar xvf ../rappor.tar.gz
-  popd
-
-  pushd _tmp/crypto
-  tar xvf ../crypto.tar.gz
-  popd
-}
-
-copy() {
-  #cp -v _tmp/chrome/byte_vector_utils* .
-  #cp -v _tmp/crypto/{hmac.h,hmac_openssl.cc} .
-  cp -v _tmp/crypto/hmac.cc .
-}
-
-count() {
-  wc -l hmac* byte_vector_utils*
 }
 
 test-hmac-sha256() {
